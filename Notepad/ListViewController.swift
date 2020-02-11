6455d0043f666f6948d2a6f4c84fd23cb3351a9f
Key5771 <ksj57715@hanmail.net>
2020-02-11 23:40:28 +0900

    Make ListTableViewCell & AddNoteViewController

--- Notepad/ListViewController.swift
+++ Notepad/ListViewController.swift
@@ -8,27 +8,68 @@
 
 import UIKit
 
-class ListViewController: UIViewController {
+class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     @IBOutlet var tableView: UITableView!
-    @IBOutlet var tableViewCell: UITableViewCell!
-    @IBOutlet var titleLabel: UILabel!
-    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var editButton: UIBarButtonItem!
+    
+    
+    // tableView cell count
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    // tablaView cell label setting
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "listContentCell", for: indexPath) as! ListTableViewCell
+        
+        cell.titleLabel.text = "Title"
+        cell.dateLabel.text = "Timestamp"
+        
+        return cell
+    }
+    
+    // tableView touched change color
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    // tableView cell swipe delete
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    // edit button click
+    @IBAction func edit(_ sender: Any) {
+        if tableView.isEditing {
+            tableView.setEditing(false, animated: true)
+            editButton.title = "편집"
+        } else {
+            tableView.setEditing(true, animated: true)
+            editButton.title = "완료"
+        }
     }
-    */
+    
+    
+    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if segue.identifier == "addNote" {
+//            let vc = segue.destination as? ViewController
+//            vc?.modalPresentationStyle = .fullScreen
+//            
+//        }
+//    }
+    
 
 }
--- Notepad/ViewController.swift
+++ Notepad/ViewController.swift
@@ -8,13 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextViewDelegate {
+    @IBOutlet var titleTextField: UITextField!
+    @IBOutlet var contentTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        titleTextField.layer.borderWidth = 1
+        titleTextField.layer.borderColor = UIColor.lightGray.cgColor
+        titleTextField.layer.cornerRadius = 5
+        
+        contentTextView.delegate = self
+        contentTextView.layer.borderWidth = 1
+        contentTextView.layer.borderColor = UIColor.lightGray.cgColor
+        contentTextView.layer.cornerRadius = 5
         // Do any additional setup after loading the view.
     }
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textViewSetupView()
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text == "" {
+            textViewSetupView()
+        }
+    }
+    
+    func textViewSetupView() {
+        if contentTextView.text == "내용입력" {
+            contentTextView.text = ""
+            contentTextView.textColor = UIColor.black
+        } else if contentTextView.text == "" {
+            contentTextView.text = "내용입력"
+            contentTextView.textColor = UIColor.lightGray
+        }
+    }
 
-
+    @IBAction func clickCancel(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
 }
 
